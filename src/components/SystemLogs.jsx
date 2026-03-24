44e658a483f1d285e215cacbb909e00a247cd215
ZydZaidan <m.yazidzaidan13@gmail.com>
2026-03-24 10:45:07 +0700

    Update 2 Indicator Icon in System Logs

--- src/components/SystemLogs.jsx
+++ src/components/SystemLogs.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-// import { FaTerminal, FaCircle } from "react-icons/fa";
 
 const SystemLogs = ({ logs }) => {
   return (
     <div className="bg-slate-900 rounded-xl p-4  border-2 border-blue-400">
-      <header className="">
+      <header className="flex items-center gap-2">
         <div className="relative flex h-2 w-2">
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
           <span className="relative inline-flex rounded-full h-2 w-2 bg-emerald-500"></span>
