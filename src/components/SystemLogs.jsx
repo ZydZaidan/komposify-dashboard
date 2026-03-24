7f4037dc534a080842197b0dfe961bb11794ece3
ZydZaidan <m.yazidzaidan13@gmail.com>
2026-03-24 10:42:22 +0700

    Update Indicator Icon in System Logs

--- src/components/SystemLogs.jsx
+++ src/components/SystemLogs.jsx
@@ -5,11 +5,15 @@ const SystemLogs = ({ logs }) => {
   return (
     <div className="bg-slate-900 rounded-xl p-4  border-2 border-blue-400">
       <header className="">
+        <div className="relative flex h-2 w-2">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-2 w-2 bg-emerald-500"></span>
+        </div>
         <p className="text-slate-400 text-xs font-bold uppercase tracking-wider ">
           System Activity Logs
         </p>
       </header>
-        <hr className="my-2 border-slate-600" />
+      <hr className="my-2 border-slate-600" />
 
       {/* ListLog */}
       <main className=" ">
